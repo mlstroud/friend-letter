f5e505815e2354ed0701c6abee8d001ae38204e7
Matt Stroud <lixingke06@gmail.com>
2020-05-18 09:07:27 -0700

    add custom souvenir

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -24,12 +24,13 @@ namespace FriendLetter.Controllers
     public ActionResult Form() { return View(); }
 
     [Route("/postcard")]
-    public ActionResult Postcard(string recipient, string sender, string location)
+    public ActionResult Postcard(string recipient, string sender, string location, string souvenir)
     {
       LetterVariable myLetterVariable = new LetterVariable();
       myLetterVariable.Recipient = recipient;
       myLetterVariable.Sender = sender;
       myLetterVariable.Location = location;
+      myLetterVariable.Souvenir = souvenir;
       return View(myLetterVariable);
     }
   }
